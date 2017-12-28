ec225d7ccf45851cdcd5eb6f52e19eedd9d6d930
rmdd <rm@dvectors.com>
2017-12-28 10:32:14 +0100

    Test added

---
+++ src/org/launchcode/Test.java
@@ -0,0 +1,9 @@
+package org.launchcode;
+
+public class Test {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello Test !");
+    }
+}
